820ccf9ad71b05aa0e4b45ff5d5224947f0d95f4
a-teresa <60651443+a-teresa@users.noreply.github.com>
2023-02-26 11:55:18 +0000

    Add files via upload

---
+++ app/config/config.php
@@ -0,0 +1,18 @@
+<?php
+//DB Params
+define('DB_HOST','localhost');
+define('DB_USER', 'root');
+define('DB_PASS','123456');
+define('DB_NAME','tvmc');
+
+//path to config.php - test
+//echo __FILE__;//test
+
+//App Root
+define('APPROOT',dirname(dirname(__FILE__)));
+
+//URL Root
+define('URLROOT', 'http://localhost/project');
+
+//Site Name
+define('SITENAME', 'project');
